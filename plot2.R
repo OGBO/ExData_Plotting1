bd53881a1e8a680501eed36f1376596f0f55a3e3
OGBO <OGBO@users.noreply.github.com>
2015-09-13 00:50:05 -0400

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,7 @@
+# Plot 2nd Graph
+plot2 <- function() {
+plot(subs$timestamp,subs$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.copy(png, file="plot2.png", width=480, height=480)
+dev.off()
+}
+> plot2()
